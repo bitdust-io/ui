4a52bdc57effc0b615733fb1d47f52be27bb8c22
Renato Cardoso <re2005@gmail.com>
2018-01-26 13:46:38 +0100

    set minumun window size

--- main.js
+++ main.js
@@ -8,7 +8,12 @@ let win;
 
 function createWindow() {
     // Create the browser window.
-    win = new BrowserWindow({width: 800, height: 600});
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        minHeight: 400,
+        minWidth: 800
+    });
 
     // and load the index.html of the app.
     win.loadURL(url.format({
