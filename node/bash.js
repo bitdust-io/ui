93d60d31788713b09ed1f5ebf5c6ec8ae33faf99
Renato Cardoso <re2005@gmail.com>
2018-02-09 12:14:04 +0100

    add gitignore

--- node/bash.js
+++ node/bash.js
@@ -2,6 +2,8 @@ const path = require('path');
 const exec = require('child_process').exec;
 const pathPrefix = process.env.ELECTRON_ENV === 'development' ? '' : process.resourcesPath;
 const scriptPath = path.resolve(pathPrefix, './scripts/deploy_osx.sh');
+// const cmd = '/usr/bin/osascript -e \'do shell script "echo some_command" with administrator privileges\'';
+// exec(cmd);
 const runBash = (cb) => exec(scriptPath, cb);
 
 module.exports = runBash;
