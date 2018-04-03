4cecb7136ca773b0f51752d45a90ced4dda07de8
Renato Cardoso <re2005@gmail.com>
2018-04-03 10:07:52 +0200

    feat(Dead): restore when connection is back alive

--- src/views/Dead.vue
+++ src/views/Dead.vue
@@ -1,13 +1,28 @@
 <template>
-    <div>
+    <div class="dead page">
         <h1>
             Ooops, something went wrong, can you please restart Bitdust
         </h1>
+        <h2>Trying to re-connect</h2>
     </div>
 </template>
 
 <script>
+    import {mapGetters} from 'vuex';
+
     export default {
-        name: 'dead'
+        name: 'dead',
+        computed: {
+            ...mapGetters([
+                'connectionStatus'
+            ])
+        },
+        watch: {
+            'connectionStatus': function (response) {
+                if (response === 'OK') {
+                    this.$router.push('home');
+                }
+            }
+        }
     };
 </script>
