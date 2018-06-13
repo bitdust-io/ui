8a1bc802a6d275388a232ae8442e9a44008984a6
Renato Cardoso <re2005@gmail.com>
2018-06-13 19:59:16 +0200

    feat(share-file): add sharing file and fileOpen visual improved

--- src/components/FileOpen.vue
+++ src/components/FileOpen.vue
@@ -9,8 +9,12 @@
                 <file-extension :file="currentFile.path"/>
                 <h1>{{currentFile.name}}</h1>
             </div>
-            <h3>File Information</h3>
+
             <div class="buttons">
+                <button @click="openShareFile(currentFile.path)"
+                        class="btn btn-terciary">
+                    Share
+                </button>
                 <button @click="downloadFile"
                         class="btn btn-primary">
                     Download
@@ -19,6 +23,40 @@
                         class="btn btn-secondary">
                     Delete
                 </button>
+
+
+                <div v-if="isSharingOpen"
+                     class="share-wrapper">
+                    <p class="share-text">Please select a friend to share this file:</p>
+
+                    <ul class="friends-list">
+                        <li v-for="friend in getFriends"
+                            @click="selectFriendToShare(friend)"
+                            :class="{active: selectedFriend.global_id === friend.global_id}">
+
+                            <user-first-letter :name="friend.username"/>
+                            {{friend.username}}
+
+                        </li>
+                    </ul>
+
+                    <div class="share-buttons">
+                        <button class="btn btn-secondary"
+                                @click="isSharingOpen = false, selectedFriend = ''">Cancel
+                        </button>
+
+                        <button class="btn btn-terciary btn-share"
+                                @click="shareFile()"
+                                :disabled="!selectedFriend">Share
+                        </button>
+                    </div>
+                </div>
+                
+                <p v-if="isSharing"
+                   class="is-sharing">
+                    Sharing file with: {{selectedFriend.username}}
+                </p>
+
                 <div v-if="downloadIsLoading">
                     Loading
                 </div>
@@ -44,23 +82,29 @@
     import {mapGetters, mapActions} from 'vuex';
     import Api from '../services/api';
     import FileExtension from '../components/FileExtension';
+    import userFirstLetter from '../components/UserFirstLetter';
 
     export default {
         data() {
             return {
                 downloadSuccess: false,
                 downloadError: false,
-                downloadIsLoading: false
+                downloadIsLoading: false,
+                isSharingOpen: false,
+                selectedFriend: '',
+                isSharing: false
 
             };
         },
         components: {
-            FileExtension
+            FileExtension,
+            userFirstLetter
         },
         methods: {
             ...mapActions([
                 'deleteFile',
-                'closeFile'
+                'closeFile',
+                'getApiFriends'
             ]),
             stopPropagation(event) {
                 event.preventDefault();
@@ -82,12 +126,29 @@
                 this.downloadSuccess = false;
                 this.downloadSuccess = false;
                 this.downloadError = false;
+                this.isSharingOpen = false;
+                this.selectedFriend = '';
+                this.isSharing = false;
+            },
+            openShareFile() {
+                this.isSharingOpen = true;
+                this.getApiFriends();
+            },
+            selectFriendToShare(friend) {
+                this.selectedFriend = friend;
+            },
+            shareFile() {
+                this.isSharing = true;
+                Api.shareFile(this.currentFile, this.selectedFriend).then(data => {
+                    this.resetOpenFile();
+                });
             }
         },
         computed: {
             ...mapGetters([
                 'isFileOpen',
-                'currentFile'
+                'currentFile',
+                'getFriends'
             ])
         },
         watch: {
@@ -114,6 +175,61 @@
 
 <style scoped lang="scss">
     @import "../../src/assets/scss/colors";
+
+    .share-buttons {
+
+        .btn {
+            padding: 6px 10px;
+            font-size: .8rem;
+        }
+
+        .btn-share {
+
+            &:disabled {
+                opacity: 0.3;
+                cursor: auto;
+            }
+        }
+    }
+
+    .is-sharing {
+        font-size: .9rem;
+        margin-top: 10px;
+        color: $color-red;
+    }
+
+    .share-wrapper {
+        margin-top: 20px;
+    }
+
+    .active {
+        padding: 3px;
+        box-shadow: inset 0 0 2px $color-green;
+
+        div {
+            background: $color-green;
+        }
+
+    }
+
+    .friends-list {
+        display: flex;
+
+        li {
+            text-align: center;
+            padding: 6px;
+            cursor: pointer;
+            transform: scale(.8);
+            border-radius: 10px;
+            > div {
+                margin: auto;
+            }
+
+            &:hover {
+                opacity: 0.6;
+            }
+        }
+    }
 
     .buttons {
         margin: 20px 0;
@@ -194,5 +310,9 @@
             background: red;
             color: $color-white;
         }
+    }
+
+    .share-text {
+        font-size: .9rem;
     }
 </style>
