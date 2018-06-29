bbf6bbf30a2ed3183cc9e3ac7a84cbb5c115affd
Renato Cardoso <re2005@gmail.com>
2018-06-29 16:19:26 +0200

    refactor(Loading): remove inline svg

--- src/views/Loading.vue
+++ src/views/Loading.vue
@@ -2,30 +2,9 @@
     <section class="bg-wave section-loading">
         <article>
             <header>
-                <div class="loading">
-                    <svg version="1.1" id="L6" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                                    viewBox="0 0 100 100" enable-background="new 0 0 100 100" xml:space="preserve">
-                        <rect fill="none" stroke="#fff" stroke-width="4" x="25" y="25" width="50" height="50">
-                            <animateTransform attributeName="transform"
-                                              dur="0.5s"
-                                                 from="0 50 50"
-                                                 to="180 50 50"
-                                                 type="rotate"
-                                                 id="strokeBox"
-                                                 attributeType="XML"
-                                                 begin="rectBox.end"/>
-                        </rect>
-                        <rect x="27" y="27" fill="#fff" width="46" height="50">
-                        <animate attributeName="height"
-                                 dur="1.3s"
-                                 attributeType="XML"
-                                 from="50"
-                                 to="0"
-                                 id="rectBox"
-                                 fill="freeze"
-                                 begin="0s;strokeBox.end"/>
-                      </rect>
-                    </svg>
+                <div class="loading-wrapper">
+                    <img src="../assets/images/loading.svg"
+                         class="loading"/>
                 </div>
                 <h1 class="page-title">
                     Connecting you
@@ -82,12 +61,7 @@
 
     .loading {
         text-align: center;
-    }
-
-    svg {
-        width: 100px;
-        height: 100px;
-        display: inline-block;
+        margin: auto;
     }
 
     .section-loading {
