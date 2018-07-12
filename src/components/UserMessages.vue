839b57532e775d2a6a7e257298fc3e1310425855
Renato Cardoso <re2005@gmail.com>
2018-07-12 23:42:57 +0200

    refactor(Chat): add timestamp and show recent messages on top

--- src/components/UserMessages.vue
+++ src/components/UserMessages.vue
@@ -10,6 +10,7 @@
             <li v-for="item in userMessages"
                 :class="{'mine': item.sender !== currentFriend.global_id}">
                 <p class="message">{{item.data.message}}</p>
+                <p class="message-time">{{new Date(item.time*1000).toLocaleString()}}</p>
                 <div class="sender" v-if="item.sender === currentFriend.global_id">
                     <user-first-letter :name="currentFriend.username"/>
                 </div>
@@ -33,11 +34,12 @@
             ]),
             userMessages() {
                 if (!this.getMessages) return;
-                return this.getMessages.filter(message => {
+                let messages = this.getMessages.filter(message => {
                     if (!message) return;
                     return message.sender.toLowerCase().includes(this.currentFriend.global_id.toLowerCase()) ||
                         message.recipient.toLowerCase().includes(this.currentFriend.global_id.toLowerCase());
                 });
+                return messages.reverse();
             }
         }
     };
@@ -47,7 +49,7 @@
     @import "../../src/assets/scss/colors";
 
     .friend-messages {
-        height: 300px;
+        height: 360px;
         padding: 20px;
     }
 
@@ -71,8 +73,8 @@
         max-width: 80%;
         border-radius: 20px;
         list-style: none;
-        font-size: .9rem;
-        padding: 8px 20px;
+        font-size: 1.1rem;
+        padding: 6px 20px;
         margin: 10px 10px 20px 20px;
         background: $color-white;
         box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.20);
@@ -94,6 +96,11 @@
     }
 
     .no-messages-here {
+        font-size: .8rem;
+    }
+
+    .message-time {
+        text-align: right;
         font-size: .8rem;
     }
 </style>
