588d1a6d71a6858fc2908183603166e94e89f8ed
Renato Cardoso <re2005@gmail.com>
2018-08-03 11:22:18 +0200

    feat(Settings): add suppliers and customers list

---
+++ src/components/Customers.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="customers">
+        My Customers
+
+        <div v-if="customers.length > 0">
+            {{customers}}
+        </div>
+        <h3 v-else>
+            You have no customers yet.
+        </h3>
+
+    </div>
+</template>
+
+<script>
+    import Api from '../services/api';
+
+    export default {
+        name: 'customers',
+        data() {
+            return {
+                customers: []
+            };
+        },
+        created() {
+            Api.getCustomers().then(data => {
+                this.customers = data.result;
+            });
+        }
+    };
+</script>
+
+<style scoped lang="scss">
+    @import "../../src/assets/scss/colors";
+
+    .customers {
+        margin: 40px 0;
+        background: $color-blue-2;
+        padding: 20px;
+    }
+
+    h3 {
+        font-size: .8rem;
+    }
+
+
+</style>
