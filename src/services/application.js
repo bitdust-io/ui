16137243e442d41fdd3ab94fcd0bfd0b53243bc6
Renato Cardoso <re2005@gmail.com>
2018-08-10 10:58:25 +0200

    refactor(application-api): remove auto Api.restartProcess()

--- src/services/application.js
+++ src/services/application.js
@@ -3,7 +3,7 @@ import message from './message';
 import store from '../store/';
 import Router from '../router';
 
-let connectionAttempts = 0;
+// let connectionAttempts = 0;
 let apiNotResponding = 0;
 
 const Application = {
@@ -12,17 +12,18 @@ const Application = {
         Api.networkConnected().then(resp => {
             store.commit('UPDATE_IS_CONNECTED', resp);
 
-            if (resp.status === 'ERROR') {
-                connectionAttempts += 1;
-                if (connectionAttempts > 20 && window.navigator.onLine) {
-                    Api.restartProcess();
-                    connectionAttempts = 0;
-                }
-            }
+            // if (resp.status === 'ERROR') {
+            //     connectionAttempts += 1;
+            //     if (connectionAttempts > 20 && window.navigator.onLine) {
+            //         Api.restartProcess();
+            //         connectionAttempts = 0;
+            //     }
+            // }
 
             setTimeout(() => {
                 this.keepConnection();
             }, 2000);
+
         }).catch(() => {
             apiNotResponding += 1;
             if (apiNotResponding > 5) {
