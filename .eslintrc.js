4bad2487c2ef307b3c1b37a9e3bfc6e3392a5692
Renato <re2005@gmail.com>
2018-11-23 11:06:43 -0200

    style(): eslint fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,12 @@ module.exports = {
     ],
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'semi': [
+            2,
+            'always'
+        ],
+        'indent': 'off'
     },
     parserOptions: {
         parser: 'babel-eslint'
