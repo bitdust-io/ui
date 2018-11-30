59ce4557eb951ee03977f2e8fd84a0b472de27eb
Renato <re2005@gmail.com>
2018-11-30 17:26:58 +0100

    feat(routers/views): add current views

---
+++ src/router/index.js
@@ -0,0 +1,61 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Loading from '../views/Loading';
+import CreateIdentity from '../views/CreateIdentity';
+import Files from '../views/Files';
+import RecoverIdentity from '../views/Recover-identity';
+import LoadingIdentity from '../views/Loading-identity';
+import Friend from '../views/Friend';
+import Dead from '../views/Dead';
+import Settings from '../views/Settings';
+
+Vue.use(Router);
+
+export default new Router({
+    routes: [
+        {
+            path: '*',
+            redirect: '/'
+        },
+        {
+            path: '/',
+            name: 'loading',
+            component: Loading
+        },
+        {
+            path: '/create-identity',
+            name: 'createIdentity',
+            component: CreateIdentity
+        },
+        {
+            path: '/recover-identity',
+            name: 'recover-identity',
+            component: RecoverIdentity
+        },
+        {
+            path: '/loading-identity',
+            name: 'loading-identity',
+            component: LoadingIdentity
+        },
+        {
+            path: '/files',
+            name: 'files',
+            component: Files
+        },
+        {
+            path: '/friend/:id',
+            name: 'friend',
+            component: Friend
+        },
+        {
+            path: '/settings',
+            name: 'settings',
+            component: Settings
+        },
+        {
+            path: '/dead',
+            name: 'dead',
+            component: Dead
+        }
+    ]
+});
---
+++ src/views/Friend.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="about">
+        <h1>Friend</h1>
+        <h2>{{$route.params.id}}</h2>
+    </div>
+</template>
