972f9637cb94192ef6ac686b2beeaa59031d34dd
Renato <re2005@gmail.com>
2018-12-21 16:20:40 +0100

    chore(): move Icon component

---
+++ src/components/Globals/BitHeader.vue
@@ -0,0 +1,115 @@
+<template>
+    <header v-if="visible">
+
+        <div id="nav">
+            <router-link v-for="(item, index) in items"
+                         :key="index"
+                         :to="item.path">
+                {{ item.name }}
+            </router-link>
+
+        </div>
+
+        <div class="header-right">
+            <user-identity/>
+            <div class="connection-status" :class="connectionStatus.status">
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+    import {mapGetters} from 'vuex';
+    import UserIdentity from './UserIdentity';
+
+    export default {
+        name: 'Header',
+        data() {
+            return {
+                visible: false
+            };
+        },
+        created() {
+            this.items = [];
+            this.$router.options.routes.forEach(route => {
+                this.items.push({
+                    name: route.name,
+                    path: route.path
+                });
+            });
+        },
+        mounted() {
+            this.visible = this.isVisible(this.$router.currentRoute.name);
+        },
+        computed: {
+            ...mapGetters([
+                'connectionStatus'
+            ])
+        },
+        methods: {
+            isVisible(name) {
+                let visibleRouters = [
+                    'files',
+                    'friend',
+                    'settings'
+                ];
+                return true;
+                // return visibleRouters.indexOf(name) > -1;
+            }
+        },
+        components: {
+            UserIdentity
+        },
+        watch: {
+            '$route'(to, from) {
+                this.visible = this.isVisible(to.name);
+            }
+        }
+    };
+</script>
+
+<style lang="scss" scoped>
+
+    @import "../../assets/scss/colors";
+
+    header {
+        height: 100px;
+        display: flex;
+        justify-content: space-between;
+
+        background-image: linear-gradient(-120deg, $color-blue-1 0%, $color-purple-1 100%);
+
+        .header-right {
+            display: flex;
+            margin-left: auto;
+            align-items: center;
+            margin-right: 15px;
+        }
+
+        .connection-status {
+            width: 20px;
+            height: 20px;
+            border: 1px solid $color-white;
+            border-radius: 50%;
+            margin-left: 10px;
+        }
+
+        .OK {
+            background: $color-green;
+        }
+        .ERROR {
+            background: red;
+        }
+    }
+
+    #nav {
+        padding: 5px;
+
+        a {
+            color: $color-white;
+            &.router-link-exact-active, &.router-link-exact-active.router-link-active {
+                color: red;
+            }
+        }
+    }
+</style>
