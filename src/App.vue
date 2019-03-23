cc5b72b69c72952cfe7257d79fb9181cdedbfdee
Renato Cardoso <re2005@gmail.com>
2019-03-23 16:29:07 +0100

    refactor(): update vue version, fix header, improve chat add icons

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,16 @@
     <div id="app">
         <BitHeader/>
         <router-view/>
-        <Bubbles/>
     </div>
 </template>
 
 <script>
     import BitHeader from '@/components/Globals/BitHeader';
-    import Bubbles from '@/components/Globals/Bubbles';
 
     export default {
         name: 'App',
         components: {
-            BitHeader,
-            Bubbles
+            BitHeader
         }
     };
 </script>
