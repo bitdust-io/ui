48361e3245c1669b5da2b02c5be7957e6eafb6f9
Renato Cardoso <re2005@gmail.com>
2019-04-02 21:25:15 +0200

    refactor(Files.vue): use grid component

--- src/views/Files.vue
+++ src/views/Files.vue
@@ -1,37 +1,43 @@
 <template>
-    <div class="files">
-        <div class="menu">
-            <ul>
+    <grid-content>
+        <div slot="menu">
+
+            <files-upload class="upload" />
+
+            <ul class="link-list">
                 <li :class="{'active': (activeTab ==='myFiles')}"
-                    @click="setMenuActive('myFiles')">
+                    @click="setMenuActive('myFiles')"
+                    class="link">
                     <font-awesome-icon icon="user-circle" />
                     MY FILES
                 </li>
 
                 <li :class="{'active': (activeTab ==='myShares')}"
-                    @click=" setMenuActive('myShares')">
+                    @click=" setMenuActive('myShares')"
+                    class="link">
                     <font-awesome-icon icon="share-alt" />
                     SHARED WITH ME
                 </li>
             </ul>
         </div>
-        <div class="main">
+        <div slot="main">
             <div v-show="this.activeTab === 'myFiles'">
-                <files-upload />
-                <file-list />
+                <file-list @open="openModal" />
             </div>
             <div v-show="this.activeTab === 'myShares'">
-                <files-shared />
+                <files-shared @open="openModal"/>
             </div>
-            <file-open />
         </div>
-    </div>
+    </grid-content>
 </template>
 <script>
+    import {mapActions} from 'vuex';
     import fileList from '../components/Files/FileList';
     import filesShared from '../components/Files/FilesShared';
     import filesUpload from '../components/Files/FileUpload';
     import fileOpen from '../components/Files/FileOpen';
+    import GridContent from '../components/Globals/GridContent';
+    import ModalService from '@/services/modal';
 
     export default {
         name: 'Files',
@@ -44,7 +50,7 @@
             fileList,
             filesShared,
             filesUpload,
-            fileOpen
+            GridContent
         },
         created() {
             document.getElementsByTagName('html')[0].classList.remove('intro-background');
@@ -52,52 +58,15 @@
         methods: {
             setMenuActive(menu) {
                 this.activeTab = menu;
+            },
+            openModal(file) {
+                ModalService.openModal({
+                    component: fileOpen,
+                    type: 'center',
+                    transitionFrom: 'top',
+                    props: {fileName: file}
+                });
             }
         }
     };
 </script>
-<style lang="scss" scoped>
-
-    @import "../assets/scss/includes.scss";
-
-    .files {
-        display: flex;
-        justify-content: space-between;
-        height: 100%;
-        margin-top: 40px;
-    }
-
-    .menu {
-        width: 250px;
-
-        ul {
-            padding: 20px;
-
-            li {
-                list-style: none;
-                cursor: pointer;
-                padding: 10px;
-                font-size: .9rem;
-                border-radius: 10px;
-
-                &.active {
-                    color: $color-white;
-                    background: $color-purple-1;
-                }
-
-                svg {
-                    margin-right: 10px;
-                }
-
-                &:hover {
-                    opacity: .8;
-                }
-            }
-        }
-    }
-
-    .main {
-        flex: 1;
-        margin: 20px 0 0 20px;
-    }
-</style>
