82f1f4ded57e53418cb070fba11770420f2de7ab
Renato Cardoso <re2005@gmail.com>
2019-04-05 14:30:15 +0200

    feat(UiStatus.vue): add

---
+++ src/components/Settings/UiStatus.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="status">
+        <div>
+            <span>State</span>
+            <span :class="status.state">{{status.state}}</span>
+        </div>
+        <div>
+            <span>Installed</span>
+            <span :class="{'active': status.installed}">{{status.installed}}</span>
+        </div>
+        <div>
+            <span>Enabled</span>
+            <span :class="{'active': status.enabled}">{{status.enabled}}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ConfigItem',
+        data() {
+            return {
+                settings: {}
+            };
+        },
+        props: {
+            status: {
+                type: Object,
+                required: true
+            }
+        }
+    };
+</script>
+
+<style lang="scss" scoped>
+    @import "@/assets/scss/includes.scss";
+
+    .status {
+        background: #ebebeb;
+        padding: 10px;
+        border-radius: 8px;
+        display: flex;
+
+        .active {
+            background: $color-blue-1;
+        }
+
+        .ON {
+            background: $color-green;
+        }
+
+        .OFF {
+            background: $color-red;
+        }
+
+        div {
+            display: flex;
+            margin: 10px;
+            border: 1px solid $color-gray-2;
+        }
+
+        span {
+            padding: 2px 8px;
+            background: white;
+            background: $color-gray-1;
+            color: $color-white;
+
+            &:first-child {
+                background: $color-gray-4;
+                color: $color-gray-1;
+                padding-right: 14px;
+                padding-left: 6px;
+            }
+        }
+    }
+
+</style>
