57480b3afa1c8b63304f98821d9fa3e5c251d8ea
Renato Cardoso <re2005@gmail.com>
2019-05-04 17:26:09 +0200

    feat(UiInput.vue): add

---
+++ src/components/Globals/UiInput.vue
@@ -0,0 +1,50 @@
+<template>
+    <label>
+        <input
+            v-model="modelValue"
+            :name="name"
+        >
+        <button @click="onChange()"
+                class="button primary is-small">save
+        </button>
+    </label>
+</template>
+
+<script>
+    export default {
+        name: 'UiInput',
+        created() {
+            this.modelValue = this.inputValue;
+        },
+        props: {
+            inputValue: {
+                type: String,
+                required: true
+            },
+            name: {
+                type: [String],
+                required: true
+            }
+        },
+        methods: {
+            onChange() {
+                this.$emit('input', this.modelValue);
+            }
+        }
+    };
+</script>
+<style lang="scss" scoped>
+    @import "../../assets/scss/includes.scss";
+
+    label {
+        display: flex;
+        margin: 10px 0;
+    }
+
+    input {
+        padding: 4px;
+        margin-right: 10px;
+        min-width: 300px;
+    }
+
+</style>
