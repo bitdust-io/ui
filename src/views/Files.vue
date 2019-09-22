a8f72f668ffadd4547fb6706261a5972ebdafeab
Renato Cardoso <re2005@gmail.com>
2019-09-22 19:52:15 +0200

    refactor: show files under keys on the menu

--- src/views/Files.vue
+++ src/views/Files.vue
@@ -2,39 +2,64 @@
     <grid-content>
         <div slot="menu">
 
-            <files-upload class="upload" />
+            <files-upload class="upload"/>
 
             <ul class="link-list">
                 <li :class="{'active': (activeTab ==='myFiles')}"
                     @click="setMenuActive('myFiles')"
                     class="link">
-                    <font-awesome-icon icon="user-circle" />
+                    <font-awesome-icon icon="user-circle"/>
                     MY FILES
+                </li>
+                <li class="keys">
+                    <span class="title">
+                        <font-awesome-icon icon="key"/>
+                        KEYS
+                    </span>
+                    <key-list @open="openKeyModal"
+                              :active-tab="activeTab"
+                              :open-key="openKey"
+                              @setKey="setOpenKey"/>
                 </li>
 
                 <li :class="{'active': (activeTab ==='myShares')}"
                     @click=" setMenuActive('myShares')"
                     class="link">
-                    <font-awesome-icon icon="share-alt" />
+                    <font-awesome-icon icon="share-alt"/>
                     SHARED WITH ME
+                </li>
+                <li class="keys">
+                    <span class="title">
+                        <font-awesome-icon icon="key"/>
+                        KEYS
+                    </span>
+                    <key-list @open="openKeyModal"
+                              :config="'share'"
+                              :active-tab="activeTab"
+                              :open-key="openKey"
+                              @setKey="setOpenKey"/>
                 </li>
             </ul>
         </div>
         <div slot="main">
-            <div v-show="this.activeTab === 'myFiles'">
-                <file-list @open="openModal" />
+            <div v-show="activeTab === 'myFiles'">
+                <file-list @open="openFileModal"
+                           :set-key="openKey"/>
             </div>
-            <div v-show="this.activeTab === 'myShares'">
-                <files-shared @open="openModal" />
+            <div v-show="activeTab === 'myShares'">
+                <files-shared @open="openFileModal"
+                              :set-key="openKey"/>
             </div>
         </div>
     </grid-content>
 </template>
 <script>
+    import keyList from '../components/Files/KeyList';
     import fileList from '../components/Files/FileList';
     import filesShared from '../components/Files/FilesShared';
     import filesUpload from '../components/Files/FileUpload';
     import fileOpen from '../components/Files/FileOpen';
+    import keyOpen from '../components/Files/KeyOpen';
     import GridContent from '../components/Globals/GridContent';
     import ModalService from '@/services/modal';
 
@@ -42,14 +67,16 @@
         name: 'Files',
         data() {
             return {
-                activeTab: 'myFiles'
+                activeTab: 'myFiles',
+                openKey: undefined
             };
         },
         components: {
             fileList,
             filesShared,
             filesUpload,
-            GridContent
+            GridContent,
+            keyList
         },
         created() {
             document.getElementsByTagName('html')[0].classList.remove('intro-background');
@@ -57,15 +84,62 @@
         methods: {
             setMenuActive(menu) {
                 this.activeTab = menu;
+                this.openKey = undefined;
             },
-            openModal(file) {
+            openFileModal(file) {
                 ModalService.openModal({
                     component: fileOpen,
                     type: 'center',
                     transitionFrom: 'top',
                     props: {fileName: file}
                 });
+            },
+            openKeyModal(key) {
+                ModalService.openModal({
+                    component: keyOpen,
+                    type: 'center',
+                    transitionFrom: 'top',
+                    props: key
+                });
+            },
+            setOpenKey(config) {
+                this.openKey = config.key;
+                this.activeTab = config.isShare ? 'myShares' : 'myFiles';
             }
         }
     };
 </script>
+
+<style scoped lang="scss">
+    @import "../assets/scss/includes.scss";
+
+    .menu {
+        .link {
+            margin: 0;
+
+            &.active {
+
+                & + .keys {
+                    border-left: 4px solid $color-purple-1;
+                }
+            }
+        }
+    }
+
+    .keys {
+        transition: border .3s ease;
+        margin-bottom: 20px;
+
+        .title {
+            margin: 0 0 10px 18px;
+            display: block;
+            font-size: .8rem;
+            padding-left: 10px;
+            color: $color-gray-1;
+
+            svg {
+                margin-right: 4px;
+            }
+        }
+    }
+</style>
