641b2a528c3482be855f0b1eecd93ebb0994739f
Renato Cardoso <re2005@gmail.com>
2019-09-22 19:52:43 +0200

    feat: keys menu and key management

---
+++ src/components/Files/KeyList.vue
@@ -0,0 +1,155 @@
+<template>
+    <ul class="user-keys">
+        <li v-for="(key, index) in shareList"
+            :key="index"
+            class="key-item"
+            :class="{'active': activeKey === key.key_id}">
+
+            <span @click="open(key)" class="edit">
+                <font-awesome-icon icon="edit"/>
+            </span>
+
+            <span class="key-name"
+                  @click="setKey(key)"
+            >{{key.label}}</span>
+        </li>
+    </ul>
+</template>
+
+<script>
+    import api from '../../services/api';
+
+    export default {
+        data() {
+            return {
+                shareList: undefined,
+                activeKey: undefined
+            };
+        },
+        props: {
+            activeTab: {
+                type: String
+            },
+            openKey: {
+                type: String
+            },
+            config: {
+                type: String
+            }
+        },
+        components: {},
+        methods: {
+            open(key) {
+                this.$emit('open', key);
+            },
+            setKey(key) {
+                this.$emit('setKey', {key: key.key_id, isShare: this.config});
+            },
+            makeConfig() {
+                let result = this.config === 'share';
+                return result ? 'granted=1&mine=0&all_customers=1' : 'granted=0&mine=1';
+            }
+        },
+        computed: {},
+        async created() {
+            const {result} = await api.getShareList(this.makeConfig());
+            this.shareList = result;
+        },
+        watch: {
+            activeTab(tab) {
+                if (tab === 'myShares') this.activeKey = undefined;
+            },
+            openKey(key) {
+                this.activeKey = key;
+            }
+        }
+    };
+</script>
+
+<style scoped lang="scss">
+    @import "../../assets/scss/includes.scss";
+
+    li {
+        width: 100%;
+        height: 30px;
+        list-style: none;
+        font-size: .8rem;
+        position: relative;
+        display: flex;
+        align-items: center;
+
+        &:before {
+            transition: width .2s ease-in-out;
+            z-index: -1;
+            position: absolute;
+            content: '';
+            height: 100%;
+            width: 0;
+            background: $color-purple-1;
+            color: $color-white;
+        }
+
+        &:hover {
+            .key-name {
+                flex: 2;
+            }
+        }
+
+        &.active {
+            color: $color-white;
+
+            &:before {
+                width: 100%;
+            }
+
+            svg {
+                color: $color-white;
+            }
+        }
+    }
+
+    .key-item {
+        margin-left: 20px;
+
+        &:hover {
+            .edit svg {
+                visibility: visible;
+                font-size: initial;
+                max-width: 30px;
+                transform: scale(1);
+            }
+        }
+    }
+
+    .key-name {
+        transition: padding .2s ease;
+        overflow: hidden;
+        max-width: 70%;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        cursor: pointer;
+        margin-left: 10px;
+        padding: 5px 0;
+
+        &:hover {
+            opacity: .6;
+        }
+    }
+
+    .edit {
+
+        &:hover {
+            opacity: .4;
+        }
+
+        svg {
+            transition: all .2s ease;
+            margin-left: 10px;
+            cursor: pointer;
+            color: $color-purple-1;
+            visibility: visible;
+            max-width: 0;
+            transform: scale(0);
+        }
+    }
+</style>
---
+++ src/components/Files/KeyOpen.vue
@@ -0,0 +1,96 @@
+<template>
+    <div class="file-open">
+        <div @click="stopPropagation($event)">
+            <div class="flex">
+                <h1><font-awesome-icon icon="key"/> {{data.label}}</h1>
+            </div>
+
+            <div class="edit-key">
+                <div v-if="!isEditing">
+                    <button class="button primary is-small"
+                            @click="edit">
+                        Edit key name
+                    </button>
+                </div>
+
+                <div v-if="isEditing">
+
+                    <input class="input"
+                           v-model="data.label">
+
+                    <button @click="cancel"
+                            class="button slim is-small">cancel
+                    </button>
+
+                    <button @click="save"
+                            class="button primary is-small">save
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import api from '../../services/api';
+
+    export default {
+        data() {
+            return {
+                isEditing: false,
+                alias: undefined
+            };
+        },
+        props: {
+            data: {
+                type: Object
+            }
+        },
+        components: {},
+        methods: {
+            stopPropagation(event) {
+                event.preventDefault();
+            },
+            edit() {
+                this.isEditing = true;
+            },
+            save() {
+                api.editKeyLabel(this.data.key_id, this.data.label);
+                this.isEditing = false;
+            },
+            cancel() {
+                this.isEditing = false;
+            }
+        },
+        mounted() {
+        }
+    };
+</script>
+
+<style scoped lang="scss">
+    @import "../../assets/scss/includes.scss";
+
+    .flex {
+        align-items: center;
+        display: flex;
+    }
+
+    h1 {
+        font-size: 1rem;
+    }
+
+    .edit-key {
+        margin-top: 20px;
+
+        .input {
+            padding: 6px;
+            font-size: .9rem;
+            display: block;
+            width: 100%;
+        }
+
+        button {
+            margin-right: 10px;
+        }
+    }
+</style>
