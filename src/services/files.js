eed6f8438edc29aeab357f7047e1d994b79fe67a
Renato Cardoso <re2005@gmail.com>
2019-09-28 11:33:44 +0200

    feat(files): add service to create file & key

---
+++ src/services/files.js
@@ -0,0 +1,67 @@
+import api from './api';
+
+const FilesService = {
+
+    async createKey(label) {
+        if (!label) {
+            const shareKeyData = await api.createFileShareKey();
+            if (shareKeyData.status !== 'OK') return false;
+            return shareKeyData.result[0].key_id;
+        } else {
+            const {result} = await api.createFileShareKey(label);
+            console.log('Key was created', result[0]);
+            return result[0];
+        }
+    },
+
+    async createPathAndUpload({getter, dispatch}, filePath, fileName, keyId) {
+        try {
+            const {result} = await api.createPath(fileName, keyId);
+            if (result.status !== 'OK') return;
+            const {status} = await api.createFile(keyId + ':' + fileName, filePath);
+            if (status === 'OK') {
+                console.log('file: ', fileName, 'Created');
+            }
+            setTimeout(() => {
+                dispatch('getApiFilesForKey', keyId);
+            }, 500);
+        } catch (e) {
+            console.log('Error trying to create path', e);
+        }
+    },
+
+    createFile({commit, dispatch, getters}, file, keyId) {
+        if (!file) return false;
+        let filePath = file.files[0].path.replace(/\\/g, '/');
+        let fileName = filePath.match(/\/([^/]*)$/)[1];
+        // TODO Sanitize file name
+
+        if (getters.hasFilePath(fileName).length === 0) {
+            if (!keyId) {
+                return api.createFileShareKey().then(shareKeyData => {
+                    if (!shareKeyData.result && shareKeyData.status !== 'OK') return false;
+                    const keyId = shareKeyData.result[0].key_id;
+                    this.createPathAndUpload({commit, dispatch, getters}, filePath, fileName, keyId);
+                });
+            } else {
+                return this.createPathAndUpload({commit, dispatch, getters}, filePath, fileName, keyId);
+            }
+        } else {
+            let currentFile = getters.hasFilePath(fileName)[0];
+            let remotePath = currentFile.key_id + ':' + currentFile.path;
+            api.createFile(remotePath, filePath).then(data => {
+                if (data.status === 'OK') {
+                    console.log('file: ', fileName, 'Created');
+                }
+            }).catch(err => {
+                console.log('error: ', err);
+            });
+            setTimeout(() => {
+                dispatch('getApiFiles');
+            }, 500);
+        }
+    }
+
+};
+
+export default FilesService;
